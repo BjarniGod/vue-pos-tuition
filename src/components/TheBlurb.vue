0796a990d14eaf7fcdc0c04337e33dcec4c3951f
BjarniGod <bgjonsson@mail.fhsu.edu>
2023-11-20 10:27:56 -0600

    Removed unnecessary data()

--- src/components/TheBlurb.vue
+++ src/components/TheBlurb.vue
@@ -6,23 +6,22 @@
     </select>
 
     <select v-if="selectedCredential === 'grad'" v-model="selectedGradProgram">
-      <option v-for="(program, name) in graduatePrograms" :value="name" :key="name">{{ program }}</option>
+      <option v-for="(program, name) in graduatePrograms" :value="name" :key="name">{{ program.title }}</option>
     </select>
 
-    <select v-if="showModeSelector" v-model="selectedMode">
-      <option value="onCampus">On-Campus</option>
-      <option value="online">Online</option>
+    <select v-if="showModeSelector" :disabled="modeDisabled" v-model="selectedMode">
+      <option v-for="mode in availableModes" :value="mode" :key="mode">{{ formatLocation(mode) }}</option>
     </select>
 
-    <div v-html="currentDescription"></div>
-    <div v-html="currentFootnote"></div>
+    <div class="tiger-text" v-html="currentDescription"></div>
+    <div class="tiger-text" v-html="currentFootnote"></div>
   </div>
 
   <TheCalculator :pricePerHour="pricePerHour" />
 </template>
 
 <script>
-import { tuitionBlurbs } from '../assets/replaceData';
+import { tuitionInfo } from '../assets/replaceData';
 import TheCalculator from './TheCalculator.vue';
 import { dollarAmount } from '../filters.js';
 
@@ -39,89 +38,72 @@ export default {
       selectedMode: 'onCampus',
       selectedCredential: 'undergrad',
       selectedGradProgram: 'grad',
-      tuitionInfo: tuitionBlurbs,
-      graduatePrograms: {
-        grad: 'Graduate',
-        mba: 'MBA',
-        mls: 'MLS',
-        mps: 'MPS',
-        msCounseling: 'MS Counseling',
-        RNtoDNP: 'RN to DNP',
-        MSNtoDNP: 'MSN to DNP'
-      },
-      programAvailability: {
-        msCounseling: 'online',
-        RNtoDNP: null,
-        MSNtoDNP: null,
-      }
+      tuitionInfo: tuitionInfo.programs,
+    }
+  },
+  computed: {
+    graduatePrograms() {
+      return Object.entries(this.tuitionInfo).reduce((acc, [key, value]) => {
+        if (key !== 'undergrad') {
+          acc[key] = value;
+        }
+        return acc;
+      }, {});
+    },
+    availableModes() {
+      const program = this.selectedCredential === 'grad' ? this.tuitionInfo[this.selectedGradProgram] : this.tuitionInfo[this.selectedCredential];
+      return program.availability;
+    },
+    showModeSelector() {
+      return this.availableModes.length > 0;
+    },
+    modeDisabled() {
+      return this.availableModes.length === 1;
+    },
+    currentProgramInfo() {
+      const programKey = this.selectedCredential === 'grad' ? this.selectedGradProgram : this.selectedCredential;
+      const program = this.tuitionInfo[programKey];
+      const tuitionData = program.tuition[this.selectedMode];
+      if (!tuitionData) return null;
+      return {
+        title: program.title,
+        price: tuitionData[0],
+        creditHours: tuitionData[1]
+      };
+    },
+    pricePerHour() {
+      const info = this.currentProgramInfo;
+      if (!info) return 0;
+      return info.price / info.creditHours;
+    },
+    currentDescription() {
+      const info = this.currentProgramInfo;
+      if (!info) return 'Information not available';
+      const template = this.selectedCredential === 'undergrad' ? this.UndergraduateText[0].description : this.GraduateText[0].description;
+      return template.replace('${tuitionCost}', dollarAmount(info.price)).replace('${location}', this.formatLocation(this.selectedMode));
+    },
+    currentFootnote() {
+      const info = this.currentProgramInfo;
+      if (!info) return 'Information not available';
+      const template = this.selectedCredential === 'undergrad' ? this.UndergraduateText[0].footnote : this.GraduateText[0].footnote;
+      return template.replace('${creditHours}', info.creditHours).replace('${pricePerHour}', dollarAmount(this.pricePerHour));
     }
   },
   methods: {
     formatLocation(mode) {
-      return mode === 'onCampus' ? 'On-Campus' : 'Online';
+      return mode === 'onCampus' ? 'On-Campus' : (mode.charAt(0).toUpperCase() + mode.slice(1));
     },
-    dollarAmount(value) {
-      if (typeof value !== "number") {
-        return value;
-      }
-      var formatter = new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD'
-      });
-      return formatter.format(value);
-    }
-  },
-  computed: {
-    showModeSelector() {
-      return this.selectedCredential !== 'grad' ||
-             (this.programAvailability[this.selectedGradProgram] !== null &&
-              this.programAvailability[this.selectedGradProgram] !== undefined);
-    },
-    modeDisabled() {
-      return this.selectedCredential === 'grad' && this.programAvailability[this.selectedGradProgram] === 'online';
-    },
-    pricePerHour() {
-      let info = this.tuitionInfo[this.selectedMode][this.selectedCredential];
-      return info[1] / info[2];
-    },
-    currentDescription() {
-      let info;
-      let descriptionTemplate;
-
-      if (this.selectedCredential === 'undergrad') {
-        info = this.tuitionInfo[this.selectedMode][this.selectedCredential];
-        descriptionTemplate = this.UndergraduateText[0].description;
-      } else {
-        info = this.tuitionInfo[this.selectedMode][this.selectedGradProgram];
-        descriptionTemplate = this.GraduateText[0].description;
-      }
-      return descriptionTemplate.replace('${tuitionCost}', dollarAmount(info[1])).replace('${location}', this.formatLocation(this.selectedMode));
-    },
-    currentFootnote() {
-      let info;
-      let footnoteTemplate;
-
-      if (this.selectedCredential === 'undergrad') {
-        info = this.tuitionInfo[this.selectedMode][this.selectedCredential];
-        footnoteTemplate = this.UndergraduateText[0].footnote;
-      } else {
-        info = this.tuitionInfo[this.selectedMode][this.selectedGradProgram];
-        footnoteTemplate = this.GraduateText[0].footnote;
-      }
-      let perHour = info[1] / info[2];
-      return footnoteTemplate.replace('${creditHours}', info[2]).replace('${pricePerHour}', this.dollarAmount(perHour));
-    }
   },
   watch: {
-    selectedGradProgram(newVal) {
-      if (this.programAvailability[newVal] === 'online') {
-        this.selectedMode = 'online';
-      } 
-      // else if (this.programAvailability[newVal] === 'onCampus') {
-      //   this.selectedMode = 'onCampus';
-      // } else {
-      //   this.selectedMode = null;
-      // }
+    selectedCredential(newVal) {
+      if (newVal === 'grad') {
+        this.selectedGradProgram = Object.keys(this.graduatePrograms)[0];
+      }
+    },
+    selectedGradProgram() {
+      if (this.availableModes.length > 0) {
+        this.selectedMode = this.availableModes[0];
+      }
     }
   }
 }
